9c6eb2e06cd2e4bde7364c5147b766b882d7d012
Hamish Buckmaster <h.buckmaster@hotmail.com>
2018-08-13 22:55:51 +1000

    Added: Flag Script
    Once the flag is planted in the ground, should stop all current movement.

---
+++ Assets/Scripts/Items/Flag.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flag : MonoBehaviour {
+
+    //glabal variables
+    public Rigidbody rb;
+
+    //functions
+    public void Start()
+    {
+        rb.GetComponent<Rigidbody>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        Debug.Log("Flag has been put into the ground");
+        rb.velocity = new Vector3(0, 0, 0);//should stop all current movement once in the ground. 
+    }
+
+}
