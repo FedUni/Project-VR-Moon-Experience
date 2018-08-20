68e65e9fe5140fcad5a7c377493827f3cd4573aa
Hamish Buckmaster <h.buckmaster@hotmail.com>
2018-08-20 16:20:05 +1000

    Fixed: Flag Script
    The flag item was staying in ground, freezing the rotation seemed to be the fix.

--- Assets/Scripts/Items/Flag.cs
+++ Assets/Scripts/Items/Flag.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Flag : MonoBehaviour {
 
-    //glabal variables
+    //global variables
     public Rigidbody rb;
 
     //functions
@@ -15,9 +15,19 @@ public class Flag : MonoBehaviour {
 
     private void OnTriggerEnter(Collider other)
     {
-        
-        Debug.Log("Flag has been put into the ground");
-        rb.velocity = new Vector3(0, 0, 0);//should stop all current movement once in the ground. 
+        if (other.tag.Equals("Ground"))
+        {
+            Debug.Log("Flag has been put into the " + other.name);
+            rb.velocity = new Vector3(0, 0, 0);//should stop all current movement once in the ground. 
+            //rb.useGravity = false;//turn off gravity once its in the ground, shouldnt move. 
+            rb.freezeRotation = true;//unfreeze rotation
+        }
+    }
+    //once leaving the ground re-apply physics
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Flag has been removed from the " + other.name);
+        rb.freezeRotation = false;
     }
 
 }
