d5342ea0be84dc369b185eadb46468acab872ab0
Kingbigdog <jackcmarshall@hotmail.com>
2018-08-29 17:33:10 +1000

    Update: Main scene/ phrase delay
    modified the Script and saved the scene

--- Assets/Scripts/phraseDelay.cs
+++ Assets/Scripts/phraseDelay.cs
@@ -2,18 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class phraseDelay : MonoBehaviour {
+public class phraseDelay : MonoBehaviour
+{
 
     AudioSource myAudio;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         myAudio = GetComponent<AudioSource>();
         myAudio.PlayDelayed(2.5f);
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
