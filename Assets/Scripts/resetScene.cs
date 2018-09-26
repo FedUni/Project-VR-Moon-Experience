999bcf03be42b7d875d2c54be2f9152d7fb961ce
Hamish Buckmaster <h.buckmaster@hotmail.com>
2018-09-26 18:17:17 +1000

    Modified: resetScene Script
    Now shows the time remaining in the inspector

--- Assets/Scripts/resetScene.cs
+++ Assets/Scripts/resetScene.cs
@@ -7,7 +7,9 @@ using UnityEngine.SceneManagement;
 public class resetScene : MonoBehaviour {
 
     public double MAX_SCENE_TIME;
+    public double TIME_REMAINING;
     private DateTime sceneStartTime;
+    
 	// Use this for initialization
 	void Start () {
         sceneStartTime = DateTime.Now;
@@ -15,7 +17,8 @@ public class resetScene : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		//check if scene time is different to max time allowed. 
+        //check if scene time is different to max time allowed. 
+        TIME_REMAINING = (sceneStartTime.AddSeconds(MAX_SCENE_TIME) - DateTime.Now).TotalSeconds;
         if(DateTime.Now >= sceneStartTime.AddSeconds(MAX_SCENE_TIME))
         {
             Debug.Log("We have reached the max scene time.");
