333849122dc14350f16a36ed1d4b1c90a239b564
Josh <powerfox1981@gmail.com>
2018-10-22 14:16:07 +1100

    Animations and click events added
    Added the animations to the experiment and camera. Added a click event to listen for player interaction (not tested yet)

---
+++ Assets/Scripts/OnClickAnimate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnClickAnimate : MonoBehaviour {
+
+    Animator anim;
+
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            anim.SetTrigger("Active");
+        }
+    }
+}
