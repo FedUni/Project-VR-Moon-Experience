3b4f5b1e46040ebc5bc62ef0abe5fc568f7175f2
Hamish Buckmaster <HamishBuckmaster@users.noreply.github.com>
2018-10-22 22:20:51 +1100

    Added: Camera Polaroid Anim, & Script Controller
    Added a animation controller and the script to make it work;
    Todo: Make it output an image and add flash

---
+++ Assets/Scripts/CameraPolaroidController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+[RequireComponent( typeof( Interactable ) )]
+public class CameraPolaroidController : MonoBehaviour {
+
+    Animator anim;
+
+    void Awake()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+    //Called every Update() while a Hand is hovering over this object
+    private void HandHoverUpdate( Hand hand )
+    {
+ 			if ( hand.GetStandardInteractionButtonDown() || ( ( hand.controller != null ) && hand.controller.GetPressDown( Valve.VR.EVRButtonId.k_EButton_Grip ) ) )
+			{
+                Debug.Log("Begin animating: " + gameObject.name + ", using animation: " + anim.name);
+                //anim.StartPlayback();
+                anim.Play("CameraButtonAnim");
+                //TODO: Add method to output polariod image
+            }
+    }
+
+}
