1246db78020b9d5324322d8944f2aff1e8562ffe
Unknown <x.rosini@hotmail.com>
2018-10-22 17:32:16 +1100

    Update: lego brick
    Scaled down

--- Assets/Scripts/OnClickAnimate.cs
+++ Assets/Scripts/OnClickAnimate.cs
@@ -5,32 +5,25 @@ using Valve.VR.InteractionSystem;
 
 public class OnClickAnimate : MonoBehaviour {
 
-    Animator anim;
+    // Animator anim;
 
-    void Awake()
-    {
-        anim = gameObject.GetComponent<Animator>();
-        Debug.Log(anim.name);
-    }
+    // void Awake()
+    // {
+    //     anim = gameObject.GetComponent<Animator>();
+    //     Debug.Log(anim.name);
+    // }
     
-    void LateUpdate()
-    {
-        if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
-            Debug.Log("fuck does it work?0");
-    }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
-            Debug.Log("fuck does it work?0");
+    // private void OnTriggerEnter(Collider other)
+    // {
 
-        Debug.Log("Colider enter");
-        if (Input.GetMouseButtonDown(1))
-        {
-            Debug.Log("button clicked");
-            //anim.SetTrigger("Active");
-            anim.Play(1);
-        }
-    }
+    //     Debug.Log("Colider enter");
+    //     if (Input.GetMouseButtonDown(1))
+    //     {
+    //         Debug.Log("button clicked");
+    //         //anim.SetTrigger("Active");
+    //         anim.Play(1);
+    //     }
+    // }
 
 }
