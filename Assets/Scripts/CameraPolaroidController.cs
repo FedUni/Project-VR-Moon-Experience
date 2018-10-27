24108c2ac6ecb0cd5ec14fbe8622ef95a3977ccd
Hamish Buckmaster <HamishBuckmaster@users.noreply.github.com>
2018-10-27 19:36:57 +1100

    Modified: Camera
    Added camera flash through a canvas that adjusts the canvas

--- Assets/Scripts/CameraPolaroidController.cs
+++ Assets/Scripts/CameraPolaroidController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Valve.VR.InteractionSystem;
+using UnityEngine.UI;
 
 [RequireComponent( typeof( Interactable ) )]
 public class CameraPolaroidController : MonoBehaviour {
@@ -9,8 +10,11 @@ public class CameraPolaroidController : MonoBehaviour {
     Animator anim;
     public GameObject polariod;
     public GameObject moveToPolariod;
+    public Canvas flashEffect;
     private Vector3 velocity = Vector3.zero;
     private bool isPrinting = false;
+    private Image flashEffectImage;
+    private bool beginFlash = false;
     void Awake()
     {
         anim = gameObject.GetComponent<Animator>();
@@ -21,28 +25,54 @@ public class CameraPolaroidController : MonoBehaviour {
  			if ( hand.GetStandardInteractionButtonDown() || ( ( hand.controller != null ) && hand.controller.GetPressDown( Valve.VR.EVRButtonId.k_EButton_Grip ) ) )
 			{
                 Debug.Log("Begin animating: " + gameObject.name + ", using animation: " + anim.name);
-                //anim.StartPlayback();
+                //anim.StartPlayback();//doesnt work?
                 anim.Play("CameraButtonAnim");
+                cameraFlash();
                 printPolaroid();
-                //TODO: Add method to output polariod image
+                
             }
             if(isPrinting)
                 printPolaroid();
+            if(beginFlash)
+                cameraFlash();
     }
 
     private void printPolaroid()
     {
         Vector3 currentPos = polariod.GetComponent<Transform>().position;
-        //polariod.GetComponent<Transform>().position = Vector3.SlerpUnclamped(new Vector3(10.0f, 0.0f, 10.0f), polariod.GetComponent<Transform>().position, 1.0f);
-        //polariod.GetComponent<Transform>().position = Vector3.Lerp(currentPos, moveToPolariod.GetComponent<Transform>().position, 1.0f);
+
         if(currentPos != moveToPolariod.GetComponent<Transform>().position)
-           { 
-               isPrinting = true;
-           }
-           else {
-                isPrinting = false;
-           }
+        { 
+            isPrinting = true;
+        }
+        else 
+        {
+            isPrinting = false;
+        }
         polariod.GetComponent<Transform>().position = Vector3.SmoothDamp(currentPos, moveToPolariod.GetComponent<Transform>().position, ref velocity, 1.0f, 0.3f);
+    }
+
+    private void cameraFlash()
+    {   
+        
+        flashEffectImage = flashEffect.GetComponentInChildren<Image>();
+        var color = flashEffectImage.color;
+        if(!beginFlash)
+        {
+        Debug.Log(flashEffect.GetComponentInChildren<Image>().color.a);
+        color.a = 1.0f;
+        beginFlash = true;
+        } 
+        else if(beginFlash || color.a == 1.0f)
+        {
+            color.a = color.a - Time.deltaTime;
+            Debug.Log(color.a);
+        }
+        if(color.a == 0.0f)
+            beginFlash = false;
+
+        flashEffectImage.color = color;//do it last
+
     }
 
 }
