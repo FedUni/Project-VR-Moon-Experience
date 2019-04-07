e78619940da328fc7ea50e0a95b735892e075081
Wayland <w.a.y.z@hotmail.com>
2019-04-07 15:58:16 +1000

    Drop rig prototype functions mocked up
    Drop Rig can now drop two objects placed on the drouble wing pairs and the objects will fall by clicking on the green panel
    The wing doors can now be reset by clicking on the yellow left pillar.

---
+++ Assets/Scripts/DropRigAnimate.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+[RequireComponent(typeof(Interactable))]
+// Modified by Wayland Bishop for The Moon VR 3.0 project
+public class DropRigAnimate : MonoBehaviour
+{
+
+    Animator anim;
+
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+    //Called every Update() while a Hand is hovering over this object
+    private void HandHoverUpdate(Hand hand)
+    {
+        GrabTypes startingGrabType = hand.GetGrabStarting();
+        if (startingGrabType != GrabTypes.None)
+        {
+            anim.StopPlayback();
+            anim.SetFloat("Direction", 1);
+            anim.Play("DropRigDropObjects");
+            Debug.Log("Begin animating: " + gameObject.name + ", using animation: " + anim.name);
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/DropRigReset.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+[RequireComponent(typeof(Interactable))]
+// Modified by Wayland Bishop for The Moon VR 3.0 project
+public class DropRigReset : MonoBehaviour
+{
+
+    Animator anim;
+
+    void Start()
+    {
+       anim = gameObject.GetComponentInParent<Animator>();
+    }
+    //Called every Update() while a Hand is hovering over this object
+    private void HandHoverUpdate(Hand hand)
+    {
+        GrabTypes startingGrabType = hand.GetGrabStarting();
+        if (startingGrabType != GrabTypes.None)
+        {
+            anim.StopPlayback();
+            anim.SetFloat("Direction", -1);
+            anim.Play("DropRigDropObjects");
+            Debug.Log("Begin animating: " + gameObject.name + ", using animation: " + anim.name + " Backwards");
+
+        }
+
+    }
+
+}
