44e89a00bc932e6dfac205b84fb678e000012e4e
hjrw <30305724@students.federation.edu.au>
2019-04-29 15:10:31 +1000

    Teleportation done
    Users can now teleport between planets. On each planet there are to soccer balls in which the user can click and teleport to a new planet. I have also added transitions between the scenes to make it look nice.

---
+++ Assets/Scripts/SceneTransitions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Valve.VR.InteractionSystem;
+
+public class SceneTransitions : MonoBehaviour
+{
+    public Animator transitionAnim;
+    public string sceneName;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(LoadScene());
+        }
+    }
+    IEnumerator LoadScene()
+    {
+        transitionAnim.SetTrigger("end");
+        yield return new WaitForSeconds(1.5f);
+        SceneManager.LoadScene(sceneName);
+    }
+
+    private void HandHoverUpdate(Hand hand)
+    {
+        GrabTypes startingGrabType = hand.GetGrabStarting();
+        if (startingGrabType != GrabTypes.None)
+        {
+            StartCoroutine(LoadScene());
+        }
+    }
+
+}
+
