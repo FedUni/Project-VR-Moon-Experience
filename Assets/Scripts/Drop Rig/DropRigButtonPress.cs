682b530cb9c2d217233f77de646fc5b29da96af4
Wayland <w.a.y.z@hotmail.com>
2019-05-06 17:20:31 +1000

    buttons fixed
    buttons fixed

--- Assets/Scripts/Drop Rig/DropRigButtonPress.cs
+++ Assets/Scripts/Drop Rig/DropRigButtonPress.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Valve.VR.InteractionSystem;
+using System;
 
 [RequireComponent(typeof(Interactable))]
 [RequireComponent(typeof(AudioSource))]
@@ -24,16 +25,37 @@ public class DropRigButtonPress : MonoBehaviour
         GrabTypes startingGrabType = hand.GetGrabStarting();
         if (startingGrabType != GrabTypes.None)
         {
-            anim.Play(gameObject.name, -1, 0); // Reset the animaiton the the button clicks again
-            anim.Play(gameObject.name); // play the button aniamtion
-            if (planetSettings.GetComponent<PlanetSettings>().hasAtmos) { // If this planet has an atmos the sound should be played
+            //anim.Play("Button", -1, 0); // Reset the animaiton the the button clicks again
+            //anim.Play("Button"); // play the button aniamtion
+
+            if (planetSettings.GetComponent<PlanetSettings>().hasAtmos)
+            { // If this planet has an atmos the sound should be played
 
                 GetComponent<AudioSource>().Play(); // Play the sound
-                GetComponent<AudioSource>().pitch = (Random.value * 0.5f + 0.5f); // Change the pitch randomly to get a better effect
-
+                GetComponent<AudioSource>().pitch = (UnityEngine.Random.value * 0.5f + 0.5f); // Change the pitch randomly to get a better effect
             }
+            AnimatorStateInfo currentState = anim.GetCurrentAnimatorStateInfo(0);
+
+            float playbackTime = currentState.normalizedTime % 1;
+            while (playbackTime < 0.5f)
+            {
+                anim.Play("Button"); // play the button aniamtion
+                StartCoroutine(MyCoroutine());
+            }
+            anim.StopPlayback();
+
+        }
+        GrabTypes endingGrabType = hand.GetGrabEnding();
+        if (endingGrabType != GrabTypes.None)
+        {
+            anim.Play("Button"); // play the button aniamtion
         }
 
+    }
+
+    IEnumerator MyCoroutine()
+    {
+        yield return 2;    //Wait one frame
     }
 
 }
