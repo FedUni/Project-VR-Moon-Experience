78dd9097a6ea1acf251d4724210dccaa3f6d40d0
Wayland <w.a.y.z@hotmail.com>
2019-05-30 21:06:16 +1000

    Major Update Still Needs Bugfix
    Teleport location fix
    Model/Texture Wristwatch
    Got rid of visor on mars
    Wrench Teleport fix
    Launch Scene Cordinates fixed
    Scene tranasition fix for HMD
    Mars Neil voice removed
    Made droppable Hammer
    Flag Fixed
    Fixed helmet view

--- Assets/Scripts/SceneTransitions.cs
+++ Assets/Scripts/SceneTransitions.cs
@@ -9,6 +9,19 @@ public class SceneTransitions : MonoBehaviour
 {
     public Animator transitionAnim;
     public string sceneName;
+    GameObject player;
+    Animator[] animators;
+    
+
+    private void Start()
+    {
+        player = GameObject.Find("Player");
+        if (PlayerPrefs.GetInt("FirstLoad") == 1) {
+            RecallPlayerPosition();
+        }
+        animators = player.GetComponentsInChildren<Animator>();
+        transitionAnim = animators[2];
+    }
 
     void Update()
     {
@@ -19,9 +32,11 @@ public class SceneTransitions : MonoBehaviour
     }
     IEnumerator LoadScene()
     {
+        
         transitionAnim.SetTrigger("end");
         yield return new WaitForSeconds(1.5f);
         SceneManager.LoadScene(sceneName);
+        
     }
 
     private void HandHoverUpdate(Hand hand)
@@ -29,9 +44,25 @@ public class SceneTransitions : MonoBehaviour
         GrabTypes startingGrabType = hand.GetGrabStarting();
         if (startingGrabType != GrabTypes.None)
         {
+            SavePlayerPosition();
+            PlayerPrefs.SetInt("FirstLoad", 1);
             StartCoroutine(LoadScene());
+
         }
     }
 
+    private void SavePlayerPosition() {
+        PlayerPrefs.SetFloat("X", player.transform.position.x);
+        PlayerPrefs.SetFloat("Y", player.transform.position.y);
+        PlayerPrefs.SetFloat("Z", player.transform.position.z);
+    }
+
+    private void RecallPlayerPosition() {
+        player.transform.position = new Vector3(PlayerPrefs.GetFloat("X"), PlayerPrefs.GetFloat("Y"), PlayerPrefs.GetFloat("Z"));
+    }
+    void OnApplicationQuit()
+    {
+        PlayerPrefs.SetInt("FirstLoad", 0);
+    }
 }
 
