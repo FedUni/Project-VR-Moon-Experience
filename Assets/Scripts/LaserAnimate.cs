1bf169513bc4e8ca69e0b22a149e39503d9306b5
Wayland <w.a.y.z@hotmail.com>
2019-05-31 14:01:03 +1000

    Laser Module laser added.
    Laser module now shows laser and outputs calculated distance to the console

--- Assets/Scripts/LaserAnimate.cs
+++ Assets/Scripts/LaserAnimate.cs
@@ -7,24 +7,43 @@ using Valve.VR.InteractionSystem;
 public class LaserAnimate : MonoBehaviour {
 
     Animator anim;
+    public MeshRenderer laserBeam;
+    GameObject planetSettings;
 
-    void Awake()
+    void Start()
     {
         anim = gameObject.GetComponent<Animator>();
+        planetSettings = GameObject.Find("PlanetSettings"); // Get the planet settings
     }
     //Called every Update() while a Hand is hovering over this object
     private void HandHoverUpdate(Hand hand)
     {
         GrabTypes startingGrabType = hand.GetGrabStarting();
         if (startingGrabType != GrabTypes.None) {
-/*        
- 		if ( hand.GetStandardInteractionButtonDown() || ( ( hand.controller != null ) && hand.controller.GetPressDown( Valve.VR.EVRButtonId.k_EButton_Grip ) ) )
-		{
-*/
             Debug.Log("Begin animating: " + gameObject.name + ", using animation: " + anim.name);
-            //anim.StartPlayback();
             anim.Play("LaserAnimate");
+            StartCoroutine(TurnOnLaser());
         }
+
+    }
+    IEnumerator TurnOnLaser()
+    {
+
+        yield return new WaitForSeconds(3.0f);
+        laserBeam.enabled = true;
+        if (planetSettings.GetComponent<PlanetSettings>().isMoon)
+        { // These set the drag up for eatch planet setting
+            Debug.Log("The distacne between the moon and the earth is 384,400 km");
+        }
+        if (planetSettings.GetComponent<PlanetSettings>().isMars)
+        {
+            Debug.Log("The distacne between the moon and the earth is 54.6 million km");
+        }
+        if (planetSettings.GetComponent<PlanetSettings>().isEarth)
+        {
+            Debug.Log("The distacne between the moon and the earth is 384,400 km");
+        }
+
 
     }
 
