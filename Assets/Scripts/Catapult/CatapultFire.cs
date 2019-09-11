6adf09e7d81817b67f12b67eb7bc5e618b50d94b
Wayland <w.a.y.z@hotmail.com>
2019-09-11 16:12:56 +1000

    Catapult Code cleaned up

--- Assets/Scripts/Catapult/CatapultFire.cs
+++ Assets/Scripts/Catapult/CatapultFire.cs
@@ -16,11 +16,9 @@ public class CatapultFire : MonoBehaviour
     bool isInterping = false;
     AnimatorStateInfo animationState;
     float aniLocation = 0;
-    //float playAmount = 0;
     public float speed = 10f;
     float animateAngle;
     bool isDoneLaunch = false;
-    bool isDoneReturn = false;
     bool beenPressed = false;
     
     void Start()
@@ -38,43 +36,18 @@ public class CatapultFire : MonoBehaviour
    
     private void Update()
     {
-        //Debug.Log("launchAngle set to " + launchAngle);
-        //Debug.Log("launchAngle set to " + launchAngle);
         if (isInterping)
         {
             Debug.Log(animateAngle);
             animationState = anim.GetCurrentAnimatorStateInfo(0);
             aniLocation = animationState.normalizedTime % 1;
             float playAmount = Mathf.Lerp(aniLocation, animateAngle, (speed * Time.deltaTime)/animateAngle);
-            //Debug.Log("playAmount set to " + playAmount);
-            //Debug.Log("aniLocation set to" + aniLocation);
-            //Debug.Log("aniLocation set to " + aniLocation);
-            //Debug.Log("launchAngle set to " + launchAngle);
-
-            //float playAmount = Mathf.Lerp(aniLocation, 0.75f, 2f * Time.deltaTime);
 
             anim.Play("CatapultAnimate", 0, playAmount);
             StartCoroutine(WaitFire());
-            //if (!isDoneLaunch) {
-                //StartCoroutine(WaitFire());
-                //isDoneLaunch = true;
-                
-            //}
-            //Debug.Log(isDoneLaunch);
-
-            //launchAngle = 0;
-            //StartCoroutine(WaitReturn());
-            //isInterping = false;
-
-            //launchAngle = 0;
-            //StartCoroutine(Wait());
-            //isInterping = false;
-            //launchAngle = oldAngle;
-
         }
         if (isDoneLaunch)
         {
-            //animateAngle = 0;
             isInterping = true;
             animationState = anim.GetCurrentAnimatorStateInfo(0);
             aniLocation = animationState.normalizedTime % 1;
@@ -83,32 +56,17 @@ public class CatapultFire : MonoBehaviour
             StartCoroutine(WaitReturn());
 
         }
-        //if (isDoneReturn)
-        //{
-            //animateAngle = launchAngle;
-            
-        //}
-
     }
     IEnumerator WaitFire()
     {
-        //Debug.Log("Going to wait 3 seconds");
         yield return new WaitForSeconds(0.5f);
-        //StartCoroutine(WaitReturn());
-        //launchAngle = oldAngle;
         isDoneLaunch = true;
-        isDoneReturn = false;
         isInterping = false;
-        
     }
     IEnumerator WaitReturn()
     {
-        //Debug.Log("Going to wait 3 seconds");
         yield return new WaitForSeconds(0.5f);
-        //StartCoroutine(WaitReturn());
-        //launchAngle = oldAngle;
         isDoneLaunch = false;
-        isDoneReturn = true;
         isInterping = false;
         beenPressed = false;
     }
@@ -122,13 +80,8 @@ public class CatapultFire : MonoBehaviour
                 Debug.Log("Pressed");
                 beenPressed = true;
                 animateAngle = launchAngle;
-                //launchAngle = oldAngle;
                 isInterping = true;
             }
-
-            
-            //anim.Play("CatapultAnimate");
-            //anim.Play("CatapultAnimate"); // Play The animation so the button goes down.
 
             if (planetSettings.GetComponent<PlanetSettings>().hasAtmos) // If this planet has an atmos the sound should be played
             {
@@ -136,11 +89,6 @@ public class CatapultFire : MonoBehaviour
                 GetComponent<AudioSource>().pitch = (UnityEngine.Random.value * 0.5f + 0.5f); // Change the pitch randomly to get a better effect
             }
         }
-    }
-
-    public void setLaunchAngle(float launchAngle)
-    {
-        this.launchAngle = launchAngle;
     }
 }
 
