250ddb09d586092b3b049c86a72a15c1bf3ab8c7
hjrw <30305724@students.federation.edu.au>
2019-09-13 13:28:18 +1000

    Putting range fixes
    Ball now stays on tee and box colliders are awesome

---
+++ Assets/Scripts/Golf Club/GolfWarning.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GolfWarning : MonoBehaviour
+{
+if (!Physics.Raycast(myray, out myhit, 1000.0f))
+  {
+      if(myhit.gameObject.tag == "Object")
+      {
+          text.setActive(true);
+          text.GetComponent<Text>().text = "text needed";
+      }
+      else
+      {
+          text.setActive(false);
+          text.GetComponent<Text>().text = "";
+      }
+  }
