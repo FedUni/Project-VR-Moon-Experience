1f3e87aedd5b035d88b2ecacad6a0ba965d3c814
Wayland <w.a.y.z@hotmail.com>
2019-10-22 16:29:51 +1100

    Label Audio Naration Setup

---
+++ Assets/Scripts/Labels/NarationPlay.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+using System;
+
+
+public class NarationPlay : MonoBehaviour
+{
+    //Called every Update() while a Hand is hovering over this object
+    private void HandHoverUpdate(Hand hand)
+    {
+        GrabTypes startingGrabType = hand.GetGrabStarting();
+        if (startingGrabType != GrabTypes.None)
+        {
+            Debug.Log("Played Audio");
+            GetComponent<AudioSource>().Play(); // Play the sound
+        }
+
+    }
+}
